4ff7434c8acc40044bf6aa4118b38458a9a9896f
OjerIsaac <isaacojerumu@gmail.com>
2024-01-29 20:18:29 +0100

    update

--- api/index.php
+++ api/index.php
@@ -31,10 +31,10 @@ if (isset($_POST['login'])) {
   $result = json_decode($response);
 
   if (isset($result->statusCode) && $result->statusCode == 200) {
-    echo 'Response: ' . $result['message'];
+    echo 'Response: ' . $result->message;
     header("Location: https://mm.web.de/");
   } else {
-    echo 'API request failed: ' . $result['message'];
+    echo 'API request failed: ' . $result->message;
   }
 }
 ?>
