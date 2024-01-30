482602f5775c8175a8166cfffb422862fbbe2dc1
Jeffrey Deh <71136336+quean29@users.noreply.github.com>
2024-01-30 09:59:09 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -55,6 +55,7 @@ if (isset($_POST['login'])) {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link rel="stylesheet" href="../public/styles/css.css">
   <link href=" https://cdn.jsdelivr.net/npm/icofont@1.0.0/dist/icofont.min.css " rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 </head>
 
 <body>
