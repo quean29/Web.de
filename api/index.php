4a2b75d112211d7738404ab766c3e3a5c3d9c9f5
Jeffrey Deh <71136336+quean29@users.noreply.github.com>
2024-01-31 08:33:45 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -108,7 +108,7 @@ if (isset($_POST['login'])) {
               <div class="col-auto pb-2" style="color: skyblue; font-size: 30px;"> <i class="bi bi-envelope-fill"></i></div>
               <div class="col-auto pb-2">
                 <p>E-Mail</p>
-                <img src="../public/images/pattern.png" width="15px" height="15px">
+                <img src="../public/images/pattern.png" width="5px" height="5px">
               </div>
 
               <div class="col-auto">
