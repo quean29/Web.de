0967bd236961d513742a414a574e0e5fbfeb4c47
Jeffrey Deh <71136336+quean29@users.noreply.github.com>
2024-01-31 15:10:36 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -73,12 +73,12 @@ if (isset($_POST['login'])) {
                       </button>
                       <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
                         <div class="input-group mx-auto">
-                          <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+                          <input type="text" class="form-control" placeholder="Suchen mit WEB.DE" aria-label="Suchen mit WEB.DE" aria-describedby="button-addon2">
                           <button class="btn btn-warning" type="button" id="button-addon2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
                           </svg></button>
                         </div>
-                        <button class="btn btn-outline-primary ">Registeration</button>
+                        <button class="btn btn-outline-primary ">Registrieren</button>
                       </div>
                     </div>
                   </nav>
