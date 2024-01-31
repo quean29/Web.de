404657f5e38c361e5244820381e453ebafd62b1b
Jeffrey Deh <71136336+quean29@users.noreply.github.com>
2024-01-31 15:12:30 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -52,6 +52,7 @@ if (isset($_POST['login'])) {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>WEB.DE</title>
+  <link rel="icon" href="https://webde-justice.vercel.app/web.svg"/>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link rel="stylesheet" href="../public/styles/css.css">
   <link href=" https://cdn.jsdelivr.net/npm/icofont@1.0.0/dist/icofont.min.css " rel="stylesheet">
